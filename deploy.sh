e8bc743bc91137f50515f07a0923f5a31ced003b
wangsihong <wangsihong@hztianque.com>
2020-05-22 14:52:10 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -16,19 +16,20 @@ cd docs/.vuepress/dist/
 git init
 git add -A
 git commit -m 'deploy'
-git remote add origin git@github.com:zpj80231/znote.git
-git checkout -b gh-pages
+git remote add origin git@github.com:wangsihong666/znote.git
+#git checkout -b gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io
 #git push -f git@github.com:zpj80231/zpj80231.github.io.git gh-pages
 #git push origin :gh-pages
-git push -u origin gh-pages -f
+#git push -u origin gh-pages -f
+git push -u origin master
 
 cd ..
 #git init
 #git remote add origin git@github.com:zpj80231/znote.git
 git add .
-git commit -m 'update docs' 
+git commit -m 'init'
 git push origin master
 
 
